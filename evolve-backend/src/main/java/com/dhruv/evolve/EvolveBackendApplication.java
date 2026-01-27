ff93bad04d9cf85bc5fd782571d0a18b845a5c42
Dhruv <156397151+DhruvChudasma91@users.noreply.github.com>
2026-01-27 15:10:39 +0530

    Initial project structure for Evolve

---
+++ evolve-backend/src/main/java/com/dhruv/evolve/EvolveBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dhruv.evolve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvolveBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvolveBackendApplication.class, args);
+	}
+
+}
---
+++ evolve-backend/src/test/java/com/dhruv/evolve/EvolveBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhruv.evolve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvolveBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
